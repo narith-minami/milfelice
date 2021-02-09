c411c1e54219bf49cead79c29b076f7f86a960ba
narith-minami <narith.for.work@gmail.com>
2021-02-09 09:54:43 +0900

    fix dependencies

---
+++ pages/consultation.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+article.page_container
+  BannerFAQ
+</template>
+
+<script>
+import BannerFAQ from "~/components/BannerFAQ.vue";
+
+export default {
+  layout: "page",
+  components: {
+    BannerFAQ,
+  },
+};
+</script>
