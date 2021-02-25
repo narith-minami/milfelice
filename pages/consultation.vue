50bece9181a5c981a93c8046cfd50b7e9ec7fdc6
Shiori <hontocha.prog@gmail.com>
2021-02-25 16:23:09 +0900

    consultation: sassのセミコロン削除

--- pages/consultation.vue
+++ pages/consultation.vue
@@ -62,93 +62,93 @@ export default {
 
 <style lang="sass" scoped>
 img
-  max-width: 100%;
+  max-width: 100%
 .consult_top
-  background-image: url('~@/assets/images/consultation/consult_back.jpg');
-  background-position: center;
-  background-color: #FFF7FA;
-  background-repeat: no-repeat;
-  height: 340px;
-  width: 100vw;
+  background-image: url('~@/assets/images/consultation/consult_back.jpg')
+  background-position: center
+  background-color: #FFF7FA
+  background-repeat: no-repeat
+  height: 340px
+  width: 100vw
   display: flex
-  align-items: center;
+  align-items: center
   &.flower_top
-    background-image: url('~@/assets/images/consultation/flower_back.jpg');
+    background-image: url('~@/assets/images/consultation/flower_back.jpg')
   img
-    max-width: 400px;
+    max-width: 400px
 .consult_inner
-  max-width: 1200px;
-  width: 100%;
-  margin: 0 auto;
+  max-width: 1200px
+  width: 100%
+  margin: 0 auto
 .consult_subtitle
-  margin: 0 0 60px;
-  color: #F59C9C;
-  font-size: 18px;
+  margin: 0 0 60px
+  color: #F59C9C
+  font-size: 18px
   text-align: center
 .consult
   &_main
-    padding: 160px 0 60px;
+    padding: 160px 0 60px
     .consult_inner
-      position: relative;
+      position: relative
       padding-top: 30px
     &_content
-      max-width: 55%;
+      max-width: 55%
     &_thumb
-      position: absolute;
-      top: 0;
-      right: 0;
-      max-width: 40%;
+      position: absolute
+      top: 0
+      right: 0
+      max-width: 40%
   &_title
-    padding-right: 40px;
-    margin-bottom: 25px;
-    text-align: center;
+    padding-right: 40px
+    margin-bottom: 25px
+    text-align: center
   &_lead
-    line-height: 1.8;
+    line-height: 1.8
     & + .consult_lead
       margin-top: 30px
   &_box
-    padding: 40px 0;
-    background-color: #FFF7FA;
+    padding: 40px 0
+    background-color: #FFF7FA
     &_title
-      display: flex;
-      justify-content: space-between;
-      max-width: 540px;
+      display: flex
+      justify-content: space-between
+      max-width: 540px
       margin-bottom: 20px
-      border-bottom: 1px solid #F59C9C;
-      font-size: 28px;
+      border-bottom: 1px solid #F59C9C
+      font-size: 28px
       span
-        font-size: 32px;
-        font-weight: bold;
+        font-size: 32px
+        font-weight: bold
     .consult_lead
-      font-size: 14px;
+      font-size: 14px
   &_btm
-    padding: 20px 0 80px;
+    padding: 20px 0 80px
   &_btn
-    max-width: 480px;
-    margin-left: auto;
+    max-width: 480px
+    margin-left: auto
   &_link
-    padding: 5px 20px;
-    border: 1px solid #EF6092;
-    border-radius: 10px;
-    font-size: 14px;
+    padding: 5px 20px
+    border: 1px solid #EF6092
+    border-radius: 10px
+    font-size: 14px
     &:hover
-      opacity: 0.8;
+      opacity: 0.8
     span
-      margin: 0 60px 0 20px;
-      line-height: 1.6;
+      margin: 0 60px 0 20px
+      line-height: 1.6
 .flower
   &_content
-    max-width: 1240px;
-    width: 100%;
-    padding: 80px 0;
-    margin: 0 auto;
+    max-width: 1240px
+    width: 100%
+    padding: 80px 0
+    margin: 0 auto
   &_title
-    padding-right: 40px;
-    margin-bottom: 25px;
-    text-align: center;
+    padding-right: 40px
+    margin-bottom: 25px
+    text-align: center
   &_list
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 15px;
-    list-style-type: none;
+    display: grid
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr))
+    grid-gap: 15px
+    list-style-type: none
 </style>
