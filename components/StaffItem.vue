5fbad6fdbe9b361dacd74db98fe4f6c3b449e785
Narith Minami <narith@Nariths-MacBook-Pro.local>
2021-05-26 21:59:01 +0900

    fix style

--- components/StaffItem.vue
+++ components/StaffItem.vue
@@ -65,6 +65,9 @@ export default {
     margin-right: auto
     margin-left: auto   
     padding: 24px 16px
+    .work_image
+      width: 125px
+      height: 125px
   @media screen and (max-width: 1000px)
     .staff_item_inner
       width: auto
